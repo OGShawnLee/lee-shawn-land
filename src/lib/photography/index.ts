4c5a083c73b13fbbf3bb354329f7052053c3574e
Shawn <changoloco.evg@gmail.com>
2025-07-30 19:59:14 +0000

    feat: update photography route

--- src/lib/photography/index.ts
+++
@@ -1,7 +0,0 @@
-export default Object.entries(
-  import.meta.glob<string>('./**/*.{jpg,png,JPG,PNG}', {
-    eager: true,
-    query: '?url',
-    import: 'default',
-  }),
-).map(it => it[1]).toSorted((a, b) => a < b ? 1 : -1);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import UnoCSS from 'unocss/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit as SvelteKit } from "@sveltejs/kit/vite"
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [UnoCSS(), SvelteKit()]
+	plugins: [enhancedImages(), UnoCSS(), SvelteKit()]
 });
